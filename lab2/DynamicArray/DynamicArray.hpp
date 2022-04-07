17a4bd0ab3de4c75870a87591fa6e7d3145c0605
Fedor <fedya.bulygin@mail.ru>
2022-04-08 00:19:11 +0300

    Dynamic Array without leaks and errors

--- lab2/DynamicArray/DynamicArray.hpp
+++ lab2/DynamicArray/DynamicArray.hpp
@@ -11,9 +11,14 @@ private:
     int size = 0;
 public:
     //Массив из k элементов
+    DynamicArray(){
+        size = 1;
+        data = (D*) new D[1];
+        memset(data, 0, sizeof(D) );
+    }
     DynamicArray(int k){
         if (k < 0){
-            cout<<"Error: array size < 0";
+            throw "Array size < 0";
         }
         size = k;
         data = (D*) new D[k];
@@ -23,38 +28,35 @@ public:
     //Массив из k элементов массива arrIn
     DynamicArray(D* arrIn, int k){
         if (k < 0){
-            cout<<"Error: array size < 0";
+            throw "Array size < 0";
         }
         size = k;
         data = (D*) new D[k];
         copy(arrIn, arrIn+k, data);
     }
     
-    //Копирующий массив конструктор
+    //Копирующий конструктор
     DynamicArray(DynamicArray<D> const &arrIn ){
-        if (arrIn.size < 0){
-            cout<<"Error: array size < 0";
-        }
         size = arrIn.size;
         data = (D*) new D[arrIn.size];
-        memset( data, 0, arrIn.size * sizeof(D) );
-        copy(arrIn.data, arrIn.data + arrIn.size, data);
+        memset( data, 0, size * sizeof(D) );
+        copy(arrIn.data, arrIn.data + min(size,arrIn.size), data);
     }
     
-    //Копирующий массив констрктор на заданном интервале
-    DynamicArray(DynamicArray<D> arrIn,int Start, int End){
-        if (arrIn.size < End){
-            throw "Error: array size < choosed interval";
+    //Копирующий констрктор на заданном интервале
+    DynamicArray(const DynamicArray<D> &arrIn,int start, int end){
+        start -= 1;
+        if (arrIn.size < end){
+            throw "Array size < choosed interval";
         }
-        size = End - Start; //Задаем размер итогового массива
+        size = end - start; //Задаем размер итогового массива
         data = (D*) new D[size];
-        
-        copy(arrIn.data + Start, arrIn.data + End, data);//Копируем интервал массива arrIn в новый массив
+        copy(arrIn.data + start, arrIn.data + end, data);//Копируем интервал массива arrIn в новый массив
     }
-    
+    //Вывод в консоль массива
     void Print(){
         if (size < 0){
-            throw "Error: array ≤ 0";
+            throw "Array size < 0";
         }
         for (int i = 0; i < size; i++){
             cout<<data[i]<<'\t';
@@ -65,30 +67,35 @@ public:
     //Получение элемента массива по индексу
     D Get(int index){
         if ( index > size ){
-            throw "IndexOutOfRange";
+            throw "Index out of range";
         }
         return data[index];
     }
 
     //Определение элемента массива по индекс
     void Set(D value, int index){
-        if (index < 0){
-            throw "Index < 0";
-        }
-        return data[index] = value;
+            if (index < 0){
+                throw "Index < 0";
+            }
+            return data[index] = value;
     }
     
     //Реалиация realloc() (изменения размера массива с сохранием элементов в нем)
     void ReSize(int nSize){
-        DynamicArray<D> dataT( *this );
-        
-        delete [] data;
-        
-        size = nSize;
+        if (nSize < 0){
+            throw "New size of array < 0";
+        }
+        D* tmp = data;
         data = (D*) new D[nSize];
-        memset(data, 0, nSize * sizeof(D) );
-        
-        copy(dataT.data,dataT.data+nSize, data);
+        memset(data,0,nSize * sizeof(D) );
+        copy(tmp,tmp + min(size, nSize), data);
+        size = nSize;
+        delete[] tmp;
     }
+
+    ~DynamicArray(){
+        delete[] data;
+    }
+    
 };
 #endif /* DynamicArray_hpp */
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,23 +1,29 @@
+#include <cstring>
 #include <iostream>
-#include "matrix.hpp"
-#include "DynamicArray.hpp"
+#include "Matrix/matrix.hpp"
+#include "DynamicArray/DynamicArray.hpp"
+#include "LinkedList/LinkedList.hpp"
 using namespace std;
 
 int main(){
     int n = 3, m = 3;
-    int arr1[5] = {1,2,3,4,5};
-    
-    matrix<int> matrix1;
-    matrix1.Print();
-    
-    DynamicArray<int> arr(arr1, 5);
+    int arr1[9] = {0,1,2,3,4,5,6,7,8};
+    DynamicArray<int> arr(arr1, 9);
     arr.Print();
-    DynamicArray<int> arra(arr1,5);
-    arra.Print();
-    arra.ReSize(10);
+    
+    DynamicArray<int> arra(arr);
     arra.Print();
     
+    DynamicArray<int> arrI(arr,4,9);
+    arrI.Print();
+    arrI.ReSize(20);
+    arrI.Print();
     
+    DynamicArray<int> arrN(arr1,5);
+    arrN.Print();
+    
+    arrN.ReSize(10);
+    arrN.Print();
     
     return 0;
 }
